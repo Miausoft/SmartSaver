bdaee79d20b9b258244def1d76c1ff14030afba7
povilasleka <povilasleka@gmail.com>
2020-09-23 22:47:42 +0300

    Created User, Transaction and Priority models

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # SmartSaver
 Trello: https://trello.com/b/amb6EPR8/smartsaver
-# SmartSaver
---
+++ SmartSaver/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartSaver.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Category { get; set; }
+        public double Amount { get; set; }
+    }
+}
