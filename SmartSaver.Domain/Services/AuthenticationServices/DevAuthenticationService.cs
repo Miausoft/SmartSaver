5fb534e9488ff52aa563505c44517c615456ddf1
povilasleka <povilasleka@gmail.com>
2020-09-30 14:12:02 +0300

    DevAuthenticationService for testing

---
+++ SmartSaver.Domain/Services/AuthenticationServices/DevAuthenticationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SmartSaver.EntityFrameworkCore;
+using SmartSaver.EntityFrameworkCore.Models;
+
+namespace SmartSaver.Domain.Services.AuthenticationServices
+{
+    class DevAuthenticationService : IAuthenticationService
+    {
+        public Account Login(string username, string _ = "")
+        {
+            using var db = new ApplicationDbContext();
+
+            var user = db.User.FirstOrDefault(p => p.Username.Equals(username));
+            return user == null ? null : db.Account.FirstOrDefault(p => p.Id.Equals(user.AccountId));
+        }
+
+        public RegistrationResult Register(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
