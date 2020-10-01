01bedba8539852b7b618df833699637c6c8a572a
povilasleka <povilasleka@gmail.com>
2020-10-01 13:59:49 +0300

    Skelet for AuthenticationService

---
+++ SmartSaver.Domain/Services/AuthenticationServices/AuthenticationService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SmartSaver.EntityFrameworkCore.Models;
+
+namespace SmartSaver.Domain.Services.AuthenticationServices
+{
+    class AuthenticationService : IAuthenticationService
+    {
+        public Account Login(string userAttribute, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RegistrationResult Register(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Hash(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Verify(string password, string hash)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
