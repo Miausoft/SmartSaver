08d8ab400bafbfa4d26b557f54ec5f0a9f4cc272
saltanovas <zumbass@gmail.com>
2020-10-08 10:30:02 +0300

    added 3 new methods for calculating user's transactions using linq
    Later on it will be transfered from TEST project

---
+++ TESTAS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TransactionCounter a = new TransactionCounter();
+            a.LoadTest();
+        }
+    }
+}
