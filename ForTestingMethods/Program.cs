551a14331b77636244dc4135e6deba76127935b9
saltanovas <zumbass@gmail.com>
2020-10-13 23:26:12 +0300

    Updated two classes with new methods and functionality
    Also added new project called ForTestingMethods that  was needed to test methods. It will be deleted in the future

---
+++ ForTestingMethods/Program.cs
@@ -0,0 +1,79 @@
+﻿using SmartSaver.EntityFrameworkCore;
+using SmartSaver.EntityFrameworkCore.Models;
+using System.Collections.Generic;
+using System;
+using SmartSaver.Domain.Services.TransactionsCounter;
+using SmartSaver.Domain.Services.SavingMethodSuggestion;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var db = new ApplicationDbContext())
+            {
+                List<Transaction> transaction = new List<Transaction>()
+                {
+                    new Transaction(){Amount = -5,      CategoryId = 1,  ActionTime = new DateTime(2020, 01, 01) },
+                    new Transaction(){Amount = -13.74,  CategoryId = 1,  ActionTime = new DateTime(2020, 01, 01) },
+                    new Transaction(){Amount = -9.07,   CategoryId = 2,  ActionTime = new DateTime(2020, 03, 03) },
+                    new Transaction(){Amount = 14.42,   CategoryId = 4,  ActionTime = new DateTime(2020, 04, 04) },
+                    new Transaction(){Amount = -14.46,  CategoryId = 1,  ActionTime = new DateTime(2020, 05, 05) },
+                    new Transaction(){Amount = 5.35,    CategoryId = 4,  ActionTime = new DateTime(2020, 06, 06) },
+                    new Transaction(){Amount = -10.92,  CategoryId = 3,  ActionTime = new DateTime(2020, 07, 07) },
+                    new Transaction(){Amount = 9.47,    CategoryId = 5,  ActionTime = new DateTime(2020, 08, 08) },
+                    new Transaction(){Amount = 14.94,   CategoryId = 4,  ActionTime = new DateTime(2020, 09, 09) },
+                    new Transaction(){Amount = 53.44,   CategoryId = 4,  ActionTime = new DateTime(2020, 10, 10) }
+                };
+
+                Account acc = new Account()
+                {
+                    GoalStartDate = new DateTime(2020, 01, 01),
+                    GoalEndDate = new DateTime(2020, 12, 21),
+                    Goal = 65,
+                    Transactions = transaction
+                };
+
+                DateTime from = new DateTime(2020, 01, 01);
+                DateTime to = new DateTime(2020, 12, 21);
+
+
+                Console.WriteLine("Kiek zmogus sutaupe pinigu kiekvienoj kategorijoj bendrai: ");
+                var dic = TransactionsCounter.TotalIncomeByCategory(transaction, from, to);
+                foreach (var d in dic)
+                {
+                    Console.WriteLine("Kategorija: " + d.Key + "     " + "Amount: " + d.Value);
+                }
+
+                Console.WriteLine();
+
+
+                Console.WriteLine("Kiek zmogus isleido pinigu kiekvienoj kategorijoj procentaliai: ");
+                var gg = TransactionsCounter.TotalExpenseByCategory(transaction, from, to);
+                foreach (var d in gg)
+                {
+                    Console.WriteLine("Kategorija: " 
+                        + d.Key 
+                        + "     " 
+                        + "Amount: " 
+                        + Math.Round(d.Value/TransactionsCounter.TotalExpense(transaction, from, to)*100, 2)
+                        );
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Ar pliuse ar minuse sedi, tai cia ale sutaupe: " + TransactionsCounter.SavedSum(transaction, from, to));
+
+                Console.WriteLine();
+                Console.WriteLine("Kiek is viso sutaupe: " + TransactionsCounter.TotalIncome(transaction, from, to));
+
+                Console.WriteLine();
+                Console.WriteLine("Kiek is viso isleido: " + TransactionsCounter.TotalExpense(transaction, from, to));
+
+                Console.WriteLine();
+                Console.WriteLine(MoneyToSpend.EstimatedTime(acc));
+            }
+        }
+    }
+}
--- SmartSaver.Domain/Services/TransactionsCounter/TransactionsCounter.cs
+++ SmartSaver.Domain/Services/TransactionsCounter/TransactionsCounter.cs
@@ -7,60 +7,59 @@ namespace SmartSaver.Domain.Services.TransactionsCounter
 {
     public static class TransactionsCounter
     {
-        /*
-         * returns list of all accounts' transactions, filtered by date
-         */
-        public static IEnumerable<Transaction> FilterAccount(IEnumerable<Transaction> transactions, int accountId, DateTime from, DateTime to)
+        public static IEnumerable<Transaction> FilterAccount(IEnumerable<Transaction> transactions, DateTime from, DateTime to)
         {
-             return transactions.Where(t => t.AccountId == accountId && InRange(t.ActionTime, from, to));
+             return transactions.Where(t => InRange(t.ActionTime, from, to));
         }
 
-        /*
-        * returns account's spent amount of money, filtered by date
-        */
-        public static double TotalAmountSpentByAccount(IEnumerable<Transaction> transaction, int accountId, DateTime from, DateTime to)
+        public static double TotalExpense(IEnumerable<Transaction> transaction, DateTime from, DateTime to)
         {
-            return transaction.Where(t => t.AccountId == accountId && t.Amount < 0 && InRange(t.ActionTime, from, to)).Sum(c => c.Amount);
+            return Math.Round(transaction.Where(t => t.Amount < 0 && InRange(t.ActionTime, from, to)).Sum(c => c.Amount), 2);
         }
 
-        /*
-        * returns account's saved amount of money, filtered by date
-        */
-        public static double TotalAmountSavedByAccount(IEnumerable<Transaction> transaction, int accountId, DateTime from, DateTime to)
+        public static double TotalIncome(IEnumerable<Transaction> transaction, DateTime from, DateTime to)
         {
-            return transaction.Where(t => t.AccountId == accountId && t.Amount > 0 && InRange(t.ActionTime, from, to)).Sum(c => c.Amount);
+            return Math.Round(transaction.Where(t => t.Amount > 0 && InRange(t.ActionTime, from, to)).Sum(c => c.Amount), 2);
         }
 
-        /*
-        * returns list of money account saved in every category, filtered by date
-        */
-        public static IEnumerable<Transaction> AmountSavedByCategory(IEnumerable<Transaction> transaction, int accountId, DateTime from, DateTime to)
+        public static double SavedSum(IEnumerable<Transaction> transaction, DateTime from, DateTime to)
         {
-            return transaction.GroupBy(t => new { t.CategoryId, t.AccountId }).Select(x => new Transaction
+            return Math.Round(TotalIncome(transaction, from, to) + TotalExpense(transaction, from, to), 2);
+        }
+
+        public static Dictionary<int, double> TotalIncomeByCategory(IEnumerable<Transaction> transaction, DateTime from, DateTime to)
+        {
+            return transaction.GroupBy(t => new { t.CategoryId, t.ActionTime }).Select(x => new Transaction
             {
                 Amount = x.Where(x => x.Amount > 0).Sum(y => y.Amount),
-                CategoryId = x.Key.CategoryId
-            }).Where(x => x.AccountId == accountId && x.Amount > 0 && InRange(x.ActionTime, from, to)).OrderBy(x => x.CategoryId);
-        }
-
-        /*
-        * returns list of money account spent in every category, filtered by date
-        */
-        public static IEnumerable<Transaction> AmountSpentByCategory(IEnumerable<Transaction> transaction, int accountId, DateTime from, DateTime to)
-        {
-            return transaction.GroupBy(t => new { t.CategoryId, t.AccountId }).Select(x => new Transaction
+                CategoryId = x.Key.CategoryId,
+                ActionTime = x.Key.ActionTime
+            }).Where(x => x.Amount > 0 && InRange(x.ActionTime, from, to)).GroupBy(x => x.CategoryId).Select(z => new Transaction
             {
-                Amount = x.Where(x => x.Amount < 0).Sum(y => y.Amount),
-                CategoryId = x.Key.CategoryId
-            }).Where(x => x.AccountId == accountId && x.Amount < 0 && InRange(x.ActionTime, from, to)).OrderBy(x => x.CategoryId);
+                Amount = z.Where(z => z.Amount > 0).Select(z => z.Amount).Sum(),
+                CategoryId = z.Select(z => z.CategoryId).First()
+            }
+            ).OrderBy(x => x.CategoryId).ToDictionary(x => x.CategoryId, x => x.Amount);
         }
 
-        /*
-        * returns if date is in range between two dates
-        */
-        public static bool InRange(this DateTime dateToCheck, DateTime startDate, DateTime endDate)
+        public static Dictionary<int, double> TotalExpenseByCategory(IEnumerable<Transaction> transaction, DateTime from, DateTime to)
+        {
+            return transaction.GroupBy(t => new { t.CategoryId, t.ActionTime }).Select(x => new Transaction
+            {
+                Amount = x.Where(x => x.Amount < 0).Select(x => x.Amount).Sum(),
+                CategoryId = x.Key.CategoryId,
+                ActionTime = x.Key.ActionTime
+            }).Where(x => x.Amount < 0 && InRange(x.ActionTime, from, to)).GroupBy(x => x.CategoryId).Select(z => new Transaction
+            {
+                Amount = z.Where(z => z.Amount < 0).Select(z => z.Amount).Sum(),
+                CategoryId = z.Select(z => z.CategoryId).First()
+            }
+            ).OrderBy(z => z.CategoryId).ToDictionary(x => x.CategoryId, x => x.Amount);
+        }
+
+        private static bool InRange(DateTime dateToCheck, DateTime startDate, DateTime endDate)
         {
             return dateToCheck >= startDate && dateToCheck < endDate;
         }
-    }
+    } 
 }
