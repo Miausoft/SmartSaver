018e7803649d91ef242d831a862d05a1e9200909
Vilius Macijauskas <macijauskas.vilius@gmail.com>
2020-10-27 18:52:22 +0200

    Created a resouce folder with a preset tip file, fixed the program link with a txt file

--- SmartSaver.Domain/Services/TipManager/Tips.cs
+++ SmartSaver.Domain/Services/TipManager/Tips.cs
@@ -7,7 +7,8 @@ namespace SmartSaver.Domain.Services.TipManager
 {
     public static class Tips
     {
-        static string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"AllTips.txt");
+        static string workingDirectory = Environment.CurrentDirectory;
+        static string path = Directory.GetParent(workingDirectory).Parent.Parent.Parent.FullName + "\\SmartSaver.Domain\\Resources\\AllTips.txt";
         static List<string> tips = System.IO.File.ReadLines(path).ToList();
 
 
