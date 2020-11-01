dcc6a008482bba486ef5888ee98d1a5b392b309b
povilasleka <povilasleka@gmail.com>
2020-11-01 13:27:38 +0200

    Transaction page simple design

---
+++ SmartSaver.MVC/Controllers/TransactionsController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SmartSaver.EntityFrameworkCore;
+using SmartSaver.EntityFrameworkCore.Models;
+using SmartSaver.MVC.Models;
+
+namespace SmartSaver.MVC.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<TransactionsController> _logger;
+
+        public TransactionsController(ApplicationDbContext context, ILogger<TransactionsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: TransactionsController
+        public ActionResult Index()
+        {
+            var model = new TransactionViewModel()
+            {
+                Transactions = _context.Transactions.Include(p => p.Category).ToList(),
+                Categories = _context.Categories.ToList()
+            };
+
+            return View(model);
+        }
+
+        // GET: TransactionsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TransactionsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind("ActionTime,Amount,ActionId,CategoryId")] Transaction transaction)
+        {
+            _context.Transactions.Add(transaction);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: TransactionsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TransactionsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SmartSaver.MVC/Models/TransactionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SmartSaver.EntityFrameworkCore.Models;
+
+namespace SmartSaver.MVC.Models
+{
+    public class TransactionViewModel
+    {
+        public List<Transaction> Transactions { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
