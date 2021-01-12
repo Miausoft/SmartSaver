ffe42303174d34fabf1750891d5e6107ff036696
saltanovas <zumbass@gmail.com>
2021-01-13 01:28:36 +0200

    simple pagination with hard coded values

--- SmartSaver.MVC/Startup.cs
+++ SmartSaver.MVC/Startup.cs
@@ -47,6 +47,8 @@ namespace SmartSaver.MVC
                 .CreateLogger();
 
             services.AddSingleton(_ => Log.Logger);
+
+            services.AddCloudscribePagination();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
