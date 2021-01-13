ffbea1c59a58cc31703663c958ba647686210e9e
saltanovas <zumbass@gmail.com>
2021-01-13 22:21:41 +0200

    changed loader code with jquery instead of vanillajs

--- SmartSaver.MVC/wwwroot/js/site.js
+++ SmartSaver.MVC/wwwroot/js/site.js
@@ -1,16 +1,7 @@
 ﻿
-window.onload = function () {
-    const preloader = document.getElementsByClassName('load')[0];
-    const inputElem = document.getElementsByTagName('form');
-    const modals = document.getElementsByClassName('close');
-    console.log(inputElem.length);
-
-    for (var i = 0; i < inputElem.length; i++) {
-        inputElem[i].addEventListener('submit', function () {
-            for (var j = 0; j < modals.length; j++) {
-                modals[j].click();
-            }
-            preloader.style.visibility = "visible";
-        });
-    }
-}
+$(document).ready(function () {
+    $("form").on('submit', function () {
+        $(".close").click();
+        $('.load').show();
+    });
+});
