626688769ae364917d80a028cb658323c7c3b576
saltanovas <zumbass@gmail.com>
2021-02-01 00:14:20 +0200

    Custom attribute to deny access for authenticated user

---
+++ SmartSaver.Domain/CustomAttributes/AnonymousOnlyAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+
+namespace SmartSaver.Domain.CustomAttributes
+{
+    public class AnonymousOnlyAttribute : ActionFilterAttribute
+    {
+        public AnonymousOnlyAttribute() { }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary(
+                        new
+                        {
+                            controller = "Account",
+                            action = "Index",
+                            returnUrl = filterContext.HttpContext.Request.Path
+                        })
+                    );
+            }
+        }
+    }
+}
