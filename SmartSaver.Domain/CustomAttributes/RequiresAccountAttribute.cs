fa7e03e0907c15d0168f833c1adcce246068194f
saltanovas <zumbass@gmail.com>
2021-02-02 14:41:33 +0200

    Throwing 404 error if user does not have such account

--- SmartSaver.Domain/CustomAttributes/RequiresAccountAttribute.cs
+++ SmartSaver.Domain/CustomAttributes/RequiresAccountAttribute.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Routing;
 using SmartSaver.Domain.Repositories;
 using SmartSaver.EntityFrameworkCore.Models;
+using System;
 using System.Linq;
 
 namespace SmartSaver.Domain.CustomAttributes
@@ -17,6 +18,11 @@ namespace SmartSaver.Domain.CustomAttributes
                 .HttpContext.RequestServices
                 .GetService(typeof(IRepository<Account>)) as Repository<Account>;
 
+            var account = accountRepo
+                .SearchFor(a => a.UserId.ToString().Equals(filterContext.HttpContext.User.Identity.Name) &&
+                                a.Name.Equals(filterContext.HttpContext.Request.Query["name"]))
+                .FirstOrDefault();
+
             if (!accountRepo.GetAll().Any(a => a.UserId.ToString().Equals(filterContext.HttpContext.User.Identity.Name)))
             {
                 filterContext.Result = new RedirectToRouteResult(
@@ -28,6 +34,10 @@ namespace SmartSaver.Domain.CustomAttributes
                             returnUrl = filterContext.HttpContext.Request.Path
                         })
                     );
+            }
+            else if (account == null || !account.UserId.ToString().Equals(filterContext.HttpContext.User.Identity.Name))
+            {
+                filterContext.HttpContext.Response.StatusCode = 404;
             }
         }
     }
