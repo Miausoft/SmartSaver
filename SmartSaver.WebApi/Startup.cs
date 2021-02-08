6c42f358c9bdd6fca9601d4786958ea05cb1807a
saltanovas <zumbass@gmail.com>
2021-02-09 01:28:26 +0200

    Enabled cors in webapi

--- SmartSaver.WebApi/Startup.cs
+++ SmartSaver.WebApi/Startup.cs
@@ -24,6 +24,14 @@ namespace SmartSaver.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins("https://localhost:44314");
+                });
+            });
+
             services.AddAutoMapper(typeof(Startup));
 
             services.AddControllers()
@@ -56,6 +64,8 @@ namespace SmartSaver.WebApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
