731c4546d2476b9d1aef594653c91d41339c5ae7
saltanovas <zumbass@gmail.com>
2021-02-12 23:40:14 +0200

    New regex for a password

--- SmartSaver.Domain/Services/Regex/PasswordRegex.cs
+++ SmartSaver.Domain/Services/Regex/PasswordRegex.cs
@@ -1,22 +1,18 @@
-﻿
-namespace SmartSaver.Domain.Services.Regex
+﻿namespace SmartSaver.Domain.Services.Regex
 {
     public class PasswordRegex : IPasswordRegex
     {
         public bool Match(string password)
         {
-            bool isValid = true;
-
             var hasNumber = new System.Text.RegularExpressions.Regex(@"[0-9]+");
             var hasUpperChar = new System.Text.RegularExpressions.Regex(@"[A-Z]+");
-            var hasMinimum5Chars = new System.Text.RegularExpressions.Regex(@".{5,}");
+            var hasLowerChar = new System.Text.RegularExpressions.Regex(@"[a-z]+");
+            var hasMinimum5Chars = password.Length >= 5;
 
-            if (!(hasNumber.IsMatch(password)
-                  && hasUpperChar.IsMatch(password)
-                  && hasMinimum5Chars.IsMatch(password)))
-                isValid = false;
-
-            return isValid;
+            return hasNumber.IsMatch(password)
+                && hasUpperChar.IsMatch(password)
+                && hasLowerChar.IsMatch(password)
+                && hasMinimum5Chars;
         }
     }
 }
