466bd60d8dfd72ad050a7b31d0c595eca465a3bb
saltanovas <zumbass@gmail.com>
2021-02-13 00:07:55 +0200

    New regex attributes

--- SmartSaver.MVC/Models/AccountViewModel.cs
+++ SmartSaver.MVC/Models/AccountViewModel.cs
@@ -8,21 +8,22 @@ namespace SmartSaver.MVC.Models
     {
         [Required]
         [MaxLength(100)]
+        [RegularExpression("^[a-zA-Z][a-zA-Z0-9]*$", ErrorMessage = "Name may only contain alphanumeric characters.")]
         [Display(Name = "Goal Name")]
         public string Name { get; set; }
 
         [Required]
-        [Range(double.Epsilon, double.MaxValue, ErrorMessage = "Invalid input")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Invalid input")]
         [Display(Name = "Amount")]
         public decimal Goal { get; set; }
 
         [Required]
-        [GreaterThanToday(ErrorMessage = "Invalid input")]
+        [GreaterThanToday(ErrorMessage = "The date must be greater than today")]
         [Display(Name = "Goal Day")]
         public DateTime GoalEndDate { get; set; }
 
         [Required]
-        [Range(double.Epsilon, double.MaxValue, ErrorMessage = "Invalid input")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Invalid input")]
         [Display(Name = "Revenue")]
         public double Revenue { get; set; }
     }
